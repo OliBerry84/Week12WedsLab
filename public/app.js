a08959790d905802e660e9651892e74c5bd502fd
Oli <oliver.berry84@gmail.com>
2018-04-18 11:29:29 +0100

    select list populated successfully

--- public/app.js
+++ public/app.js
@@ -1,8 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
   const url ='https://restcountries.eu/rest/v2/all';
 
-  const button = document.querySelector('#button');
-  button.addEventListener('click', () => {
-    makeRequest(url, requestComplete);
-  });
+  makeRequest(url, requestComplete);
+
+  // const select = document.querySelector('#country-list');
+  // select.addEventListener('click', () => {
+  //   makeRequest(url, requestComplete);
+  // });
 });
+
+const makeRequest = function (url, callback) {
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.send();
+  request.addEventListener('load', callback);
+}
+const requestComplete = function () {
+  if (this.status !== 200) return;
+  const jsonString = this.responseText;
+  const countries = JSON.parse(jsonString);
+  populateList(countries);
+}
+const populateList = function (countries) {
+  const select = document.querySelector('#country-list');
+  countries.forEach((country) => {
+    const option = document.createElement('option');
+    option.textContent = country.name;
+    select.appendChild(option);
+  });
+}
